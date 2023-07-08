cc0c81503cdaac8848dbcccfb8cecc7b6c0a5ac2
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-07-08 11:33:43 +0300

    Initial Commit

---
+++ GuessThePic/GuessThePicApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessThePicApp.swift
+//  GuessThePic
+//
+//  Created by Sergey Hrabrov on 08.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessThePicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
