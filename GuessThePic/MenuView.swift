8f2182b6a3a864674d79bed9e75b8397d30df3b9
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-07-09 02:08:15 +0300

    main: Edited Logo

--- GuessThePic/MenuView.swift
+++ GuessThePic/MenuView.swift
@@ -9,69 +9,81 @@ import SwiftUI
 
 struct MenuView: View {
     @ObservedObject var matchManager: MatchManager
-    @State var logoGradientColors: [Color] = [.purple, .yellow, .pink, .purple]
-    @State private var animateGradient: Bool = false
+    let gradientColors: [Color] = [.purple, .yellow, .pink, .purple]
+    @State private var animationAmount: CGFloat = -1
     
     var body: some View {
         VStack {
             Spacer()
-            
-            //            Image("logo")
-            //                .resizable()
-            //                .scaledToFit()
-            //                .padding(30)
-            Text("Guess What")
-                .font(Font.system(size: 84, weight: .bold))
-                .multilineTextAlignment(.center)
+
+            ZStack {
+                        Text("Guess\nWhat")
+                            .font(.system(size: 96))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.clear)
+                            .multilineTextAlignment(.center)
+                            .background(
+                                GeometryReader { geometry in
+                                    LinearGradient(gradient: Gradient(colors: gradientColors), startPoint: .init(x: 0, y: 0.5), endPoint: .init(x: 1, y: 0.5))
+                                        .frame(width: geometry.size.width)
+                                        .offset(x: geometry.size.width * animationAmount)
+                                        .onAppear() {
+                                            withAnimation(Animation.linear(duration: 3).repeatForever(autoreverses: false)) {
+                                                animationAmount = 1
+                                            }
+                                        }
+                                }
+                            )
+                            .mask(
+                                Text("Guess\nWhat")
+                                    .font(.system(size: 96))
+                                    .fontWeight(.heavy)
+                                    .multilineTextAlignment(.center)
+                            )
+
+                        Text("Guess\nWhat")
+                            .font(.system(size: 96))
+                            .fontWeight(.heavy)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .opacity(0.5)
+                    }
+        
+        
+        
+        
+        Spacer()
+        
+        Button {
+            print("Start")
+        } label: {
+            Text("PLAY")
                 .foregroundColor(.white)
-                .mask(
-                    LinearGradient(
-                        gradient: Gradient(colors: logoGradientColors),
-                        startPoint: animateGradient ? .leading : .trailing,
-                        endPoint: animateGradient ? .trailing : .leading
-                    )
-                )
-                .animation(
-                    Animation.linear(duration: 3)
-                        .repeatForever(autoreverses: false)
-                )
-                .onAppear() {
-                    animateGradient = true
-                }
-            
-            
-            Spacer()
-            
-            Button {
-                print("Start")
-            } label: {
-                Text("PLAY")
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .bold()
-            }
-            .disabled(matchManager.authenticationState != .authenticated || matchManager.inGame)
-            .padding(.vertical, 20)
-            .padding(.horizontal, 100)
-            .background(
-                Capsule(style: .circular)
-                    .fill(matchManager.authenticationState != .authenticated || matchManager.inGame
-                          ? .gray : Color("primaryYellow"))
-                
-            )
-            
-            Text(matchManager.authenticationState.rawValue)
-            //                .font(.headline.weight(.semibold))
-                .font(Font.system(size: 20, weight: .semibold))
-                .padding()
-                .foregroundStyle(
-                    LinearGradient(colors: [.yellow, .purple, .yellow, .yellow], startPoint: .leading, endPoint: .trailing)
-                )
-            
-            Spacer()
-            
-            
+                .font(.largeTitle)
+                .bold()
         }
+        .disabled(matchManager.authenticationState != .authenticated || matchManager.inGame)
+        .padding(.vertical, 20)
+        .padding(.horizontal, 100)
+        .background(
+            Capsule(style: .circular)
+                .fill(matchManager.authenticationState != .authenticated || matchManager.inGame
+                      ? .gray : Color("primaryYellow"))
+            
+        )
+        
+        Text(matchManager.authenticationState.rawValue)
+        //                .font(.headline.weight(.semibold))
+            .font(Font.system(size: 20, weight: .semibold))
+            .padding()
+            .foregroundStyle(
+                LinearGradient(colors: [.yellow, .purple, .yellow, .yellow], startPoint: .leading, endPoint: .trailing)
+            )
+    
+        Spacer()
+        
+        
+    }
         .background(
             Image("menuBg")
                 .resizable()
@@ -79,7 +91,7 @@ struct MenuView: View {
                 .scaleEffect(1.1)
         )
         .ignoresSafeArea()
-    }
+}
 }
 
 
