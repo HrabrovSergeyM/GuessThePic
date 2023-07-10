59f8307f77d4bad7c93b05682a089f502b92ebfd
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-07-10 10:57:55 +0300

    main: Added GameView, DrawingView

--- GuessThePic/GameView.swift
+++ GuessThePic/GameView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct GameView: View {
     @ObservedObject var matchManager: MatchManager
     @State var drawingGuess = ""
-    @State var eraserEnabled = ""
+    @State var eraserEnabled = false
     
     func makeGuess() {
         
@@ -28,12 +28,55 @@ struct GameView: View {
                     topBar
                     
                     ZStack {
+                        DrawingView(matchManager: matchManager, eraserEnabled: $eraserEnabled)
+                            .aspectRatio(1, contentMode: .fit)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color("primaryPurple"), lineWidth: 10)
+                            )
                         
+                        VStack {
+                            HStack {
+                                Spacer()
+                                
+                                if matchManager.currentlyDrawing {
+                                    Button {
+                                        eraserEnabled.toggle()
+                                    } label: {
+                                        Image(systemName: eraserEnabled ? "eraser.fill" : "eraser")
+                                            .font(.title)
+                                            .foregroundColor(Color("primaryPurple"))
+                                            .padding(.top, 10)
+                                    }
+                                }
+                            }
+                            
+                            Spacer()
+                            
+                            
+                            
+                        }
+                        .padding()
                     }
                     
+                    pastGuesses
                 }
+                .padding(.horizontal, 30)
+                .ignoresSafeArea(.keyboard, edges: .bottom)
+                
             }
+            
+            VStack {
+                Spacer()
+                
+                promptGroup
+                
+                
+            }
+            .ignoresSafeArea(.container)
+            
         }
+        
     }
     
     var topBar: some View {
@@ -58,6 +101,97 @@ struct GameView: View {
             }
         }
         .padding(.vertical, 15)
+    }
+    
+    var pastGuesses: some View {
+        ScrollView {
+            ForEach(matchManager.pastGuesses) { guess in
+                HStack {
+                    Text(guess.message)
+                        .font(.title2)
+                        .bold(guess.correct)
+                    
+                    if guess.correct {
+                        Image(systemName: "hand.thumbsup.fill")
+                            .foregroundColor(matchManager.currentlyDrawing ?
+                                             Color(red: 0.808, green: 0.345, blue: 0.776) :
+                                                Color(red: 0.243, green: 0.773, blue: 0.745)
+                            )
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, 1)
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(
+            matchManager.currentlyDrawing ?
+            Color("primaryYellow") :
+                Color("primaryPurple")
+            
+        )
+        .opacity(0.5)
+        .cornerRadius(20)
+        .padding(.vertical)
+        .padding(.bottom, 130)
+        
+    }
+    var promptGroup: some View {
+        VStack {
+            if matchManager.currentlyDrawing {
+                Label("DRAW:", systemImage: "exclamationmark.bubble.fill")
+                    .font(.title2)
+                    .bold()
+                    .foregroundColor(.white)
+                Text(matchManager.drawPrompt.uppercased())
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                    .foregroundColor(.white)
+            } else {
+                HStack {
+                    Label("GUESS THE DRAWING!", systemImage: "exclamationmark.bubble.fill")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                }
+                
+                HStack {
+                    TextField("Type your Guess", text: $drawingGuess)
+                        .padding()
+                        .background(
+                            Capsule(style: .circular)
+                                .fill(.white)
+                        )
+                        .onSubmit(makeGuess)
+                    
+                    Button {
+                        makeGuess()
+                    } label: {
+                        Image(systemName: "chevron.right.circle.fill")
+                            .renderingMode(.original)
+                            .foregroundColor(Color("primaryPurple"))
+                            .font(.system(size: 50))
+                    }
+                }
+                
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding([.horizontal, .bottom], 30)
+        .padding(.vertical)
+        .background(
+            (matchManager.currentlyDrawing ?
+             Color("primaryYellow") :
+                Color("primaryPurple")
+            )
+            .opacity(0.5)
+            //            .brightness(-0.1)
+        )
+        
     }
 }
 
